749412aa3b1540b5d525df597ad72edea1410eea
wanghaichao <wanghaichao@didachuxing.com>
2022-02-24 11:22:53 +0800

    init

---
+++ app/.cxx/cmake/release/armeabi-v7a/CMakeFiles/3.10.2/CMakeSystem.cmake
@@ -0,0 +1,24 @@
+set(CMAKE_HOST_SYSTEM "Darwin-20.6.0")
+set(CMAKE_HOST_SYSTEM_NAME "Darwin")
+set(CMAKE_HOST_SYSTEM_VERSION "20.6.0")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/Users/dida/Develop/android-ndk-r15c/build/cmake/android.toolchain.cmake")
+
+set(CMAKE_SYSTEM "Android-16")
+set(CMAKE_SYSTEM_NAME "Android")
+set(CMAKE_SYSTEM_VERSION "16")
+set(CMAKE_SYSTEM_PROCESSOR "armv7-a")
+
+set(CMAKE_ANDROID_NDK "/Users/dida/Develop/android-ndk-r15c")
+set(CMAKE_ANDROID_STANDALONE_TOOLCHAIN "")
+set(CMAKE_ANDROID_ARCH "arm")
+set(CMAKE_ANDROID_ARCH_ABI "armeabi-v7a")
+set(CMAKE_ANDROID_ARCH_HEADER_TRIPLE "arm-linux-androideabi")
+set(CMAKE_ANDROID_NDK_DEPRECATED_HEADERS "0")
+set(CMAKE_ANDROID_ARM_MODE "1")
+set(CMAKE_ANDROID_ARM_NEON "0")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
